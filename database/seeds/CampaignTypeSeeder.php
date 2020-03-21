aba8bdcdf28e98960f78fc13791da434ac5a665e
Michael Barquero <mike@mb.cr>
2020-03-21 20:12:50 +0000

    Adding initial files, including db tables and seeds and also auth

---
+++ database/seeds/CampaignTypeSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CampaignTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('campaign_type')->insert([
+            'campaign_type_name' => 'Campaign Type 1',
+            'campaign_type_description' => 'This is the campaign type #1',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('campaign_type')->insert([
+            'campaign_type_name' => 'Campaign Type 2',
+            'campaign_type_description' => 'This is the campaign type #2',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        DB::table('campaign_type')->insert([
+            'campaign_type_name' => 'Campaign Type 3',
+            'campaign_type_description' => 'This is the campaign type #3',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
